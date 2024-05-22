e949ab0eee938edaaa77cf58b8680cdd97b1d17f
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-05-22 00:37:42 -0600

    whitespace

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const colorClasses : {[key: string]: string} = {
   'ra-red': 'bg-primary hover:bg-white hover:text-primary hover:border-primary hover:border-2',
   'ra-orange': 'bg-secondary-orange hover:bg-white hover:text-secondary-orange hover:border-secondary-orange hover:border-2',
-  'ra-gradient': 'bg-gradient-to-r from-primary to-secondary-orange ',
+  'ra-gradient': 'bg-gradient-to-r from-primary to-secondary-orange',
 }
 
 function Button({children, color='ra-red', className='', ...props}: {children: React.ReactNode, color?: string, className?: string, [key: string]: any}) {
