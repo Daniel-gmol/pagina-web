a6b34919a3da6c8cce8f6ea79310e7f9be278474
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-05-26 10:09:33 -0600

    meta viewport update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import Head from 'next/head'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+      <meta name="viewport" content="viewport-fit=cover, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
