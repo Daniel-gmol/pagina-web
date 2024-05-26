940d7002379f61b632c9ba753f48f1b4cb7ef004
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-05-26 10:13:15 -0600

    new main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,111 +1,37 @@
-import React, { useEffect } from 'react'
-import axios, { AxiosError } from 'axios'
-import Button from '@/components/Button'
-import Input from '@/components/Input'
+import { withAuth } from '../lib/auth';
+import React, { useEffect } from 'react';
 
-function login() {
-  useEffect(() => {
-    document.title  = 'Log in'  
-  }, [])
+function HomePage({isAuthenticated}: {isAuthenticated: boolean}) {
 
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+    useEffect(() => {
+        document.title = 'Home';
+        if (isAuthenticated) {
+            setTimeout(() => {
+                window.location.href = '/menu';
+            }, 750);
+        }
+    }, [isAuthenticated]);
 
-  //es un poco de desastre jajaja
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    console.log("entro")
-
-    // Check if username or password is empty
-    if (!username || !password) {
-      return;
-    } else {
-      console.log('Username:', username);
-      console.log('Password:', password);
-    } 
-  
-    try {
-
-
-      const response  = await axios.post(
-        'https://rockwell-pl.onrender.com/api/login', {
-          email : username,
-          password_ : password
-        });
-        console.log(response)
-    
-      if (response.status === 200) {
-        window.location.href = '/menu';
-      } else {
-        console.error('Failed to log in');
-      }
-    } catch (error) {
-      const err = error as AxiosError
-      console.error(err.response?.data)
-    }
-  };
-
-
-  return (
-    <div className='container-log-in'>
-
-      <img className='img-log-in' 
-           alt='logo'
-           src='/RA-LOGO-PLACEHOLDER.png' 
-      />
-    
-      <h1 className='h1-log-in'>
-        Welcome !
-      </h1>
-
-      <form className='form-log-in'
-            onSubmit={handleLogin}
-      >
-        <Input className=''
-                classInput='input-component' 
-                title='Email' 
-                type='text'
-                autocomplete='username'
-                value={username}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setUsername(e.target.value);
-                  // console.log('Username:', e.target.value);
-                }}
-        />  
-        <Input className='mt-10'
-                classInput='input-component'
-                title='Password'
-                type='password'
-                autocomplete='current-password'
-                value={password}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setPassword(e.target.value);
-                  // console.log('Password:', e.target.value);
-                }}
-        />
-
-        <a href='./forgot' 
-          className='text-primary hover:underline mt-3'>
-          Forgot your password?
-        </a>
-
-        <Button className='border-2 min-w-32 min-h-12'>Log in</Button>
-        <Button className='border-2 min-w-32 min-h-12' 
-          onClick={(event: { preventDefault: () => void }) => { 
-            event.preventDefault(); 
-            window.location.href = '/menu'; 
-          }}
-        >Guest</Button>
-      </form>
-
-      <p>Don't have an account?  
-        <a href='./signin' className='text-primary hover:underline'>
-           Sign up
-        </a>
-      </p>
-    </div>
-  )
+    return (
+        <div className='flex flex-col gap-5 items-center justify-center h-full'>
+            <h1>Welcome</h1>
+            <div role="status">
+                <svg aria-hidden="true" className="w-10 h-10 animate-spin text-secondary-grey fill-primary" viewBox="0 0 100 101" fill="none">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+            </div>
+        </div>
+    );
 }
 
-export default login
+
+
+export async function getServerSideProps(context: any) {
+    const { req } = context;
+    const { cookie } = req.headers;
+    return withAuth(context, cookie);
+}
+
+
+export default HomePage;
