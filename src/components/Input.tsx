53e8692516821b4ac7289df9968ceb211123c523
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-05-26 10:09:13 -0600

    Input Error Handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Input({className='', classInput='', classLabel='', ...props}) {
     const divClasses = 
-        `inline-block
+        `inline-block h-12
         ${className}`
     
     const inputClasses = 
@@ -27,7 +27,18 @@ function Input({className='', classInput='', classLabel='', ...props}) {
                 autoComplete={props.autocomplete}
                 value={props.value}
                 onChange={props.onChange}
-                />
+                onBlur={props.onBlur}
+        />
+        {!props.isValid && <small className="error-text">{props.errorMessage}</small>}
+        {props.title === 'Password' && (
+            <button
+                type="button"
+                className={`float-right text-right' ${!props.isValid ? 'mt-[-1.7rem] mr-[-8.75rem]' : 'mt-[-1.7rem]'}`}
+                onClick={props.toggleShowPassword}
+            >
+            {props.showPassword ? 'HIDE' : 'SHOW'}
+            </button>
+        )}
     </div>
   )
 }
